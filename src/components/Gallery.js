451946e3d5c3a08e299d601f959c071e02e90636
Leszek Michno <leszek.michno@gmail.com>
2023-06-26 15:42:53 +0200

    carousel gallery added

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,10 +3,6 @@ import { data } from "../databases/ImgBase";
 
 const Gallery =()=> {
 
-  // const captionStyle = {
-  //   fontSize: '2em',
-  //   fontWeight: 'bold',
-  // }
   const slideNumberStyle = {
     fontSize: '20px',
     fontWeight: 'bold',
@@ -22,19 +18,12 @@ const Gallery =()=> {
             time={5000}
             width="850px"
             height="500px"
-            // captionStyle={captionStyle}
-            // radius="10px"
             slideNumber={true}
             slideNumberStyle={slideNumberStyle}
-            // captionPosition="bottom"
             automatic={true}
             dots={true}
             pauseIconColor="white"
             pauseIconSize="40px"
-            // slideBackgroundColor="black"
-            // slideImageFit="cover"
-            // thumbnails={true}
-            // thumbnailWidth="100px"
             style={{
               textAlign: "center",
               maxWidth: "850px",
